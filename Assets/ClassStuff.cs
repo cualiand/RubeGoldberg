61c46e904e1f679290688f5eda89206afd4b7b60
Andrew Cualing <ayc300@nyu.edu>
2015-02-23 06:16:06 -0500

    repo created, gitignore added

---
+++ Assets/ClassStuff.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ClassStuff : MonoBehaviour {
+
+	public float speed = 5f;
+	//this is a public variable, so that it can be called later in start/updated
+	//also shows it in the inspector to be messed with
+
+	//use public variables to get objects to communicate with each other
+	public Text uiText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetKey (KeyCode.W)) {
+			transform.position += transform.forward * Time.deltaTime * speed;
+			uiText.text = "I'm moving forward!";
+		}
+
+		if ( Input.GetKey (KeyCode.S)) {
+			transform.position -= transform.forward * Time.deltaTime * speed;
+			uiText.text = "I'm moving backward!";
+		}
+	}
+}
